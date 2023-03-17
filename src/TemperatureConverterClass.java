6490a2af68aa6ebc7533e98e1c781dbb1e1d33e5
Achraf-Youssef <100535820+Achraf-Youssef@users.noreply.github.com>
2023-03-17 11:11:40 +0100

    update

--- src/TemperatureConverterClass.java
+++ src/TemperatureConverterClass.java
@@ -1,2 +1,22 @@
 public class TemperatureConverterClass {
+    private final String fromTo;
+    private final double ratio;
+
+    TemperatureConverterClass(String fromTo, double ratio) {
+        this.fromTo = fromTo;
+        this.ratio = ratio;
+    }
+
+    public String getFromTo() {
+        return fromTo;
+    }
+
+    public double convertFromTo(double temp) {
+        return temp * ratio;
+    }
+
+    @Override
+    public String toString() {
+        return fromTo;
+    }
 }
--- src/TemperatureConverterProgram.java
+++ src/TemperatureConverterProgram.java
@@ -1,2 +1,26 @@
-public class TemperatureConverterProgram {
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class TemperatureConverterProgram extends JFrame implements ActionListener {
+    private final JButton convertButton= new JButton("convert");
+    private final JTextField inputTextField = new JTextField();
+    private final JTextField resultTextField = new JTextField();
+
+    public TemperatureConverterProgram() {
+        // title
+        super("Temperature Convertor V1.0");
+
+        // setup
+        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        this.setLocationRelativeTo(null);
+        this.setSize(400, 150);
+        this.setResizable(false);
+        this.setVisible(true);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
 }
