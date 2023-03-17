a76d5eb11159f422031134bc394e70ed5c61e921
Achraf-Youssef <100535820+Achraf-Youssef@users.noreply.github.com>
2023-03-17 12:07:53 +0100

    finished

--- src/TemperatureConverterProgram.java
+++ src/TemperatureConverterProgram.java
@@ -1,4 +1,5 @@
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
@@ -6,10 +7,68 @@ public class TemperatureConverterProgram extends JFrame implements ActionListene
     private final JButton convertButton= new JButton("convert");
     private final JTextField inputTextField = new JTextField();
     private final JTextField resultTextField = new JTextField();
+    private final JComboBox<TemperatureConverterClass>  comboBox = new JComboBox<>(new TemperatureConverterClass[] {
+        new TemperatureConverterClass("Celsius->Fahrenheit", 0) {
+            @Override
+            public double convertFromTo(double temp) {
+                return (temp * (9 / 5.0)) + 32;
+            }
+        },
+        new TemperatureConverterClass("Fahrenheit->Celsius", 0) {
+            @Override
+            public double convertFromTo(double temp) {
+                return (temp - 32) * (5.0 / 9);
+            }
+        },
+        new TemperatureConverterClass("Celsius->Kelvin", 0) {
+            @Override
+            public double convertFromTo(double temp) {
+                return temp + 273.15;
+            }
+        },
+        new TemperatureConverterClass("Kelvin->Celsius", 0) {
+            @Override
+            public double convertFromTo(double temp) {
+                return temp - 273.15;
+            }
+        },
+        new TemperatureConverterClass("Fahrenheit->Kelvin", 0) {
+            @Override
+            public double convertFromTo(double temp) {
+                return ((temp - 32) * (5.0 / 9)) + 273.15;
+            }
+        },
+        new TemperatureConverterClass("Kelvin->Fahrenheit", 0) {
+            @Override
+            public double convertFromTo(double temp) {
+                return ((temp - 273.15) * (9 / 5.0)) + 32;
+            }
+        },
+    });
 
     public TemperatureConverterProgram() {
         // title
         super("Temperature Convertor V1.0");
+        this.setLayout(new GridLayout(3, 2, 5, 5));
+
+        // local variables
+        JLabel inputLabel = new JLabel("enter the temperature:");
+        JLabel resultLabel = new JLabel("result:");
+
+        // init
+        convertButton.setFocusable(false);
+        resultTextField.setEditable(false);
+
+        // adding ActionListeners to the components
+        convertButton.addActionListener(this);
+
+        // adding components to the frame
+        this.add(inputLabel);
+        this.add(inputTextField);
+        this.add(resultLabel);
+        this.add(resultTextField);
+        this.add(comboBox);
+        this.add(convertButton);
 
         // setup
         this.setDefaultCloseOperation(EXIT_ON_CLOSE);
@@ -21,6 +80,29 @@ public class TemperatureConverterProgram extends JFrame implements ActionListene
 
     @Override
     public void actionPerformed(ActionEvent e) {
+        if (e.getSource() == convertButton) {
+            resultTextField.setText("");
 
+            TemperatureConverterClass selectedItem = (TemperatureConverterClass) comboBox.getSelectedItem();
+
+            if (selectedItem != null) {
+                String[] units = selectedItem.getFromTo().split("->");
+                String c2 = units[1];
+
+                try {
+                    double input = Double.parseDouble(inputTextField.getText());
+
+                    input = selectedItem.convertFromTo(input);
+
+                    resultTextField.setText(String.format("%.3f %s", input, c2));
+                    inputTextField.setText("");
+                } catch (NumberFormatException ne) {
+                    JOptionPane.showMessageDialog(this,
+                            "error\n" + ne.getMessage(),
+                            "error",
+                            JOptionPane.ERROR_MESSAGE);
+                }
+            }
+        }
     }
 }
